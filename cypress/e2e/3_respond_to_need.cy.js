a107d48f995f744480f14323c28f47480eaa8651
zAukaRS <evgenii-savostin10@mail.ru>
2025-05-12 20:34:40 +0700

    Response

--- cypress/e2e/3_respond_to_need.cy.js
+++ cypress/e2e/3_respond_to_need.cy.js
@@ -2,16 +2,23 @@ describe('Просмотр и фильтрация потребностей ст
     it('Авторизация, фильтрация, проверка радиокнопок, диапазона и сброса фильтров', () => {
         cy.visit('https://dev.profteam.su/login');
 
-        cy.get('input[placeholder="Латинские символы"]').type('zauka');
+        cy.get('input[placeholder="Латинские символы"]').type('ZaukaRS');
         cy.get('input[placeholder="*******"]').type('Dead10dead');
         cy.get('button').contains('Войти').click();
 
+        cy.url().should('include', '/account/main');
+
         cy.get('a[href="/needs"]').should('be.visible').contains('Потребности').click();
 
-        // Убедимся, что перешли на нужную страницу
-        cy.url().should('include', '/needs');
+        cy.url({ timeout: 10000 }).should('include', '/needs');
 
+        cy.get('.need-item', { timeout: 10000 }).should('exist');
 
+        cy.get('.need-item').filter((index, el) => {
+            return Cypress.$(el).find('button:contains("Откликнуться")').length > 0;
+        }).first().within(() => {
+            cy.contains('button', 'Откликнуться').click();
+        });
 
     });
 });
